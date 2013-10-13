a8c9881fac89c7c3398e055fad1bfb64aad82f98
Jelle Versele <jelle@versele.be>
2013-10-13 22:55:15 +0200

    Added some unit tests.

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,17 @@
-test( "hello test", function() {
-  ok( 1 == "1", "Passed!" );
+var $nav, nav;
+
+$nav = $('#primaryNav');
+$nav.mobileNav();
+nav = $nav.data('plugin_mobileNav');
+	
+test( "open test", function() {
+	nav.open();
+	ok($nav.hasClass('mobileNavOpen'));
 });
+
+test( "destruct test", function() {
+	nav.close();
+	ok(! $nav.hasClass('mobileNavOpen'));
+});
+
+	
